211f666be7caf3b491c400d8f3596e3cbc75f435
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-15 15:32:27 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+    
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+        //console.log(responce)
+    .then(data => {console.log(data)
+    
+        const dogList = document.getElementById('dog-image-container');
+        data.message.forEach(dog => {
+            const img = document.createElement('img');
+            img.src = dog;
+            dogList.appendChild(img);
+        })
+    })
+    .catch(error => console.log(error))
+})
